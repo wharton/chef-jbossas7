b6ae4e4bb5aeb1f1ab59ed0c8b9c25963cbcb281
Brian Flad <bflad417@gmail.com>
2012-10-19 16:15:39 -0400

    Sub-classed JBossAS in libary name. Fixed license documentation header.

--- libraries/jbossas.rb
+++ libraries/jbossas.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: wharton-jboss-eap6
-# Recipe:: default
+# Library:: jbossas
 #
 # Copyright 2012
 #
@@ -17,77 +17,79 @@
 # limitations under the License.
 #
 
-class Chef::Recipe::JBossEAP6::JBossAS
-  def self.add_domain_slaves_mgmt_users
-    domain_slaves.each do |domain_slave|
-      add_hostname_mgmt_user(domain_slave)
+class Chef::Recipe::JBossEAP6
+  class JBossAS
+    def self.add_domain_slaves_mgmt_users
+      domain_slaves.each do |domain_slave|
+        add_hostname_mgmt_user(domain_slave)
+      end
     end
-  end
 
-  def self.add_hostname_mgmt_user(node2)
-    node2_hostname = node2["jboss-eap6"]["jbossas"]["hostname"]
-    @node["jboss-eap6"]["jbossas"]["mgmt-users"]["#{node2_hostname}"] = node2["jboss-eap6"]["jbossas"]["mgmt-users"]["#{node2_hostname}"]
-  end
-
-  def self.create_hostname_mgmt_user
-    Chef::Log.info("Creating JBoss AS mgmt-user for domain authentication")
-    require 'base64'
-    require 'digest'
-    require 'securerandom'
-    password = SecureRandom.urlsafe_base64(16)
-    new_hostname_mgmt_user = {
-      "password" => Digest::MD5.hexdigest(password),
-      "secret" => Base64.strict_encode64(password)
-    }
-    hostname_mgmt_user = new_hostname_mgmt_user
-    new_hostname_mgmt_user
-  end
-
-  def self.domain_master?
-    domain_master?(@node)
-  end
-
-  def self.domain_master?(node)
-    domain_mode?(node) && node["jboss-eap6"]["jbossas"]["domain"]["host_type"] == "master"
-  end
-  
-  def self.domain_mode?
-    domain_mode?(@node)
-  end
-
-  def self.domain_mode?(node)
-    node["jboss-eap6"]["jbossas"]["mode"] == "domain"
-  end
-
-  def self.domain_slave?
-    domain_slave?(@node)
-  end
-  
-  def self.domain_slave?(node)
-    domain_mode?(node) && node["jboss-eap6"]["jbossas"]["domain"]["host_type"] == "slave"
-  end
-
-  def self.domain_slaves
-    slave_nodes = []
-    search(:node, "chef_environment:#{@node.chef_environment} AND recipes:wharton-jboss-eap6") do |jboss_node|
-      slave_nodes << jboss_node if domain_slave?(jboss_node) && in_same_domain?(jboss_node)
+    def self.add_hostname_mgmt_user(node2)
+      node2_hostname = node2["jboss-eap6"]["jbossas"]["hostname"]
+      @node["jboss-eap6"]["jbossas"]["mgmt-users"]["#{node2_hostname}"] = node2["jboss-eap6"]["jbossas"]["mgmt-users"]["#{node2_hostname}"]
     end
-    slave_nodes
-  end
 
-  def self.has_domain_master?
-    domain_slave? && @node["jboss-eap6"]["jbossas"]["domain"]["master"]["address"]
-  end
+    def self.create_hostname_mgmt_user
+      Chef::Log.info("Creating JBoss AS mgmt-user for domain authentication")
+      require 'base64'
+      require 'digest'
+      require 'securerandom'
+      password = SecureRandom.urlsafe_base64(16)
+      new_hostname_mgmt_user = {
+        "password" => Digest::MD5.hexdigest(password),
+        "secret" => Base64.strict_encode64(password)
+      }
+      hostname_mgmt_user = new_hostname_mgmt_user
+      new_hostname_mgmt_user
+    end
 
-  def self.hostname_mgmt_user
-    hostname_mgmt_user(@node)
-  end
+    def self.domain_master?
+      domain_master?(@node)
+    end
 
-  def self.hostname_mgmt_user(node)
-    node["jboss-eap6"]["jbossas"]["mgmt-users"]["#{node["jboss-eap6"]["jbossas"]["hostname"]}"]
-  end
+    def self.domain_master?(node)
+      domain_mode?(node) && node["jboss-eap6"]["jbossas"]["domain"]["host_type"] == "master"
+    end
+    
+    def self.domain_mode?
+      domain_mode?(@node)
+    end
 
-  def self.in_same_domain?(node2)
-    @node["jboss-eap6"]["jbossas"]["domain"]["name"] == node2["jboss-eap6"]["jbossas"]["domain"]["name"]
+    def self.domain_mode?(node)
+      node["jboss-eap6"]["jbossas"]["mode"] == "domain"
+    end
+
+    def self.domain_slave?
+      domain_slave?(@node)
+    end
+    
+    def self.domain_slave?(node)
+      domain_mode?(node) && node["jboss-eap6"]["jbossas"]["domain"]["host_type"] == "slave"
+    end
+
+    def self.domain_slaves
+      slave_nodes = []
+      search(:node, "chef_environment:#{@node.chef_environment} AND recipes:wharton-jboss-eap6") do |jboss_node|
+        slave_nodes << jboss_node if domain_slave?(jboss_node) && in_same_domain?(jboss_node)
+      end
+      slave_nodes
+    end
+
+    def self.has_domain_master?
+      domain_slave? && @node["jboss-eap6"]["jbossas"]["domain"]["master"]["address"]
+    end
+
+    def self.hostname_mgmt_user
+      hostname_mgmt_user(@node)
+    end
+
+    def self.hostname_mgmt_user(node)
+      node["jboss-eap6"]["jbossas"]["mgmt-users"]["#{node["jboss-eap6"]["jbossas"]["hostname"]}"]
+    end
+
+    def self.in_same_domain?(node2)
+      @node["jboss-eap6"]["jbossas"]["domain"]["name"] == node2["jboss-eap6"]["jbossas"]["domain"]["name"]
+    end
   end
 end
